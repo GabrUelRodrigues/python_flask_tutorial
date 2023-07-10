b9606a2bf6b74e3ed126c863cf88df1441dc263f
GabrUelRodrigues <gabruel.rodrigues2001@gmail.com>
2023-07-10 16:35:34 -0300

    Login and Sign Up pages and POST request setup

--- website/auth.py
+++ website/auth.py
@@ -2,14 +2,14 @@ from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/sign-up", methods=["GET", "POST"])
+def sign_up():
+	return render_template("sign_up.html")
+
+@auth.route("/login", methods=["GET", "POST"])
 def login():
 	return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
 	return "<h1>Logout</h1>"
-
-@auth.route("/sign-up")
-def sign_up():
-	return render_template("sign_up.html")

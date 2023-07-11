06ee5270c55915433be6dd90445de9b0c6c3bbf2
GabrUelRodrigues <gabruel.rodrigues2001@gmail.com>
2023-07-11 02:26:11 -0300

    Creating new user accounts

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint("auth", __name__)
 
@@ -23,8 +26,12 @@ def sign_up():
 			flash("Passwords don't match.", category="error")
 
 		else:
-			#Add user to the database
+			new_user = User(email=email, name=name, password=generate_password_hash(password1, method="scrypt"))
+			db.session.add(new_user)
+			db.session.commit()
 			flash("Account created!", category="success")
+			
+			return redirect(url_for("views.home"))
 
 	return render_template("sign_up.html")
 
